1d203509b4cf7fbbf7ba1007f45a600c777a0a12
scaleup <nkoroieric@gmail.com>
2018-11-26 10:33:36 +0300

    changes to add screenshots

---
+++ sampleApp/src/main/java/com/scaleup/admin/curvedbottomnavigation/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.scaleup.admin.curvedbottomnavigation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.widget.NestedScrollView
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
